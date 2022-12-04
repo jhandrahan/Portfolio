88cb81963f1a8bfc15f06a9e40fd7c760dc0e08b
jhandrahan <jessicahandrahan@gmail.com>
2022-12-03 20:15:02 -0400

    changed some javascript on hamburger menu and styled button

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
-const app = {};
+// const app = {};
 
-app.button = document.getElementById("hamburgerButton");
-console.log(app.button);
+// app.button = document.getElementById("hamburgerButton");
+// // console.log(app.button);
 
-app.mobileNav = document.querySelector(".desktopMenu");
-console.log(app.mobileNav);
+// app.mobileNav = document.querySelector(".desktopMenu");
+// // console.log(app.mobileNav);
 
-app.button.addEventListener("click", function () {
+// app.button.addEventListener("click", function () {
 
-    app.mobileNav.classList.toggle('show');
+//     app.mobileNav.classList.toggle('show');
 
+//     // app.button.classList.toggle('show');
+// })
+// document.querySelectorAll(".link").forEach(n => n.addEventListener('click', () => {
+//     console.log(app.button);
+//     app.button.classList.remove('show')
+// }))
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".desktopMenu")
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("show");
+    navMenu.classList.toggle("show");
 })
+
+document.querySelectorAll(".link").forEach(n => n.addEventListener('click', () => {
+    hamburger.classList.remove("show");
+    navMenu.classList.remove("show");
+}))
