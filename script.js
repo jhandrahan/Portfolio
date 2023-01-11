1d89962674ad2a4a8b94fb5030366db0a2dffbea
jhandrahan <jessicahandrahan@gmail.com>
2023-01-10 22:29:42 -0400

    deleted unneccesary images, also update color of p text and fixed mobile sizing on contact form

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-// const app = {};
-
-// app.button = document.getElementById("hamburgerButton");
-// // console.log(app.button);
-
-// app.mobileNav = document.querySelector(".desktopMenu");
-// // console.log(app.mobileNav);
-
-// app.button.addEventListener("click", function () {
-
-//     app.mobileNav.classList.toggle('show');
-
-//     // app.button.classList.toggle('show');
-// })
-// document.querySelectorAll(".link").forEach(n => n.addEventListener('click', () => {
-//     console.log(app.button);
-//     app.button.classList.remove('show')
-// }))
 
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".desktopMenu")
