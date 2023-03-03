419f12fec5d335c063338eb480f23fee4feadbac
jhandrahan <jessicahandrahan@gmail.com>
2023-03-02 21:40:08 -0400

    formatted files

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
-
 const hamburgerButton = document.querySelector("#hamburger");
 const closeHamburgerButton = document.querySelector(".closeMenu");
-const hamburgerMenu = document.querySelector('.slideOutNav');
-const navOptions = document.querySelector('.desktopMenu');
+const hamburgerMenu = document.querySelector(".slideOutNav");
+const navOptions = document.querySelector(".desktopMenu");
 
-hamburgerButton.addEventListener('click', (event) => {
-    hamburgerMenu.classList.add('open');
+hamburgerButton.addEventListener("click", (event) => {
+  hamburgerMenu.classList.add("open");
 });
 
-closeHamburgerButton.addEventListener('click', (event) => {
-    hamburgerMenu.classList.remove('open');
+closeHamburgerButton.addEventListener("click", (event) => {
+  hamburgerMenu.classList.remove("open");
 });
 
-navOptions.addEventListener('click', (event)=>{
-    hamburgerMenu.classList.remove('open');
+navOptions.addEventListener("click", (event) => {
+  hamburgerMenu.classList.remove("open");
 });
-
-
